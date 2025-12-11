91f3ddc1ed3b6c20e71ab59f2c071db70c50c75c
Sarvesh Neelraj <sarveshneel@gmail.com>
2025-12-11 19:20:23 +0400

    site is now live on nest!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
-import netlify from '@astrojs/netlify';
-
 import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://tarfish.github.io',
-
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
+  output: 'server',
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
